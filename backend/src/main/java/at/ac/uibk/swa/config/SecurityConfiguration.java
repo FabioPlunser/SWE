bb8f47ef0a49218b7e77dca7f248c231ff1526ac
davirieser <david.w.rieser@gmail.com>
2023-03-10 15:10:35 +0100

    Added Reflection for getting Public Endpoints in SecurityConfiguration

--- backend/src/main/java/at/ac/uibk/swa/config/SecurityConfiguration.java
+++ backend/src/main/java/at/ac/uibk/swa/config/SecurityConfiguration.java
@@ -2,10 +2,11 @@ package at.ac.uibk.swa.config;
 
 import at.ac.uibk.swa.config.exception_handling.RestAccessDeniedHandler;
 import at.ac.uibk.swa.config.filters.HeaderTokenAuthenticationFilter;
-import at.ac.uibk.swa.config.filters.CookieTokenAuthenticationFilter;
-import at.ac.uibk.swa.models.Permission;
+import at.ac.uibk.swa.models.annotations.PublicEndpoint;
 import at.ac.uibk.swa.util.EndpointMatcherUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,7 +19,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.*;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.util.pattern.PathPattern;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Class for configuring the Authentication Process of the Web-Server.
@@ -41,10 +49,14 @@ public class SecurityConfiguration {
     private AuthenticationProvider provider;
 
     @Autowired
+    private StartupConfig.Profile profile;
+
+    @Autowired
     private AuthenticationFailureHandler failureHandler;
 
     @Autowired
-    private EndpointMatcherUtil endpointMatcherUtil;
+    @Qualifier("requestMappingHandlerMapping")
+    private RequestMappingHandlerMapping handlerMapping;
     //endregion
 
     //region Authentication Manager Bean
@@ -58,19 +70,10 @@ public class SecurityConfiguration {
     //endregion
 
     //region Custom Authentication Filter Beans
-    @Bean
-    AbstractAuthenticationProcessingFilter bearerAuthenticationFilter(HttpSecurity http) throws Exception {
-        final AbstractAuthenticationProcessingFilter filter = new HeaderTokenAuthenticationFilter(endpointMatcherUtil.getProtectedApiRoutes());
-
-        filter.setAuthenticationManager(authManager(http));
-        filter.setAuthenticationFailureHandler(failureHandler);
-
-        return filter;
-    }
-
-    @Bean
-    AbstractAuthenticationProcessingFilter cookieAuthenticationFilter(HttpSecurity http) throws Exception {
-        final AbstractAuthenticationProcessingFilter filter = new CookieTokenAuthenticationFilter(endpointMatcherUtil.getAdminRoutes());
+    AbstractAuthenticationProcessingFilter bearerAuthenticationFilter(
+            HttpSecurity http, final RequestMatcher requiresAuth
+    ) throws Exception {
+        final AbstractAuthenticationProcessingFilter filter = new HeaderTokenAuthenticationFilter(requiresAuth);
 
         filter.setAuthenticationManager(authManager(http));
         filter.setAuthenticationFailureHandler(failureHandler);
@@ -79,36 +82,120 @@ public class SecurityConfiguration {
     }
     //endregion
 
+    //region PublicEndpoint Matchers
+    private RequestMatcher getPublicEndpointMatcher() {
+        return new OrRequestMatcher(getPublicEndpointStream().toArray(RequestMatcher[]::new));
+    }
+
+    private Stream<RequestMatcher> getPublicEndpointStream() {
+        return getPublicEndpointStrings().map(AntPathRequestMatcher::new);
+    }
+
+    private Stream<String> getPublicEndpointStrings() {
+        // NOTE: This is a workaround because Spring Security does not define an Annotation
+        //       that lets you exempt an Endpoint from the FilterChain.
+        // Get all Controller defined Endpoints that Spring knows about.
+        Map<RequestMappingInfo, HandlerMethod> methods = handlerMapping.getHandlerMethods();
+        return methods.entrySet().stream()
+                // Filter for Endpoints annotated with the PublicEndpoint Annotation
+                .filter(h -> h.getValue().hasMethodAnnotation(PublicEndpoint.class))
+                // Get the full Endpoint String (including any Prefixes from @RequestMapping's on the Controller)
+                .flatMap(h -> Stream.of(h.getKey()
+                        .getPathPatternsCondition()
+                        .getPatterns().stream()
+                        .map(PathPattern::getPatternString)
+                        .toArray(String[]::new)))
+                // Ensure that no one accidentally unlocks multiple Endpoints at once
+                .filter(p -> !p.endsWith("*"));
+    }
+    //endregion
+
+    //region Actuator Endpoint Matchers
+    private RequestMatcher getActuatorEndpointMatcher() {
+        // Enable the Actuator Endpoints in the Development Environment
+        if (this.profile == StartupConfig.Profile.DEBUG) {
+            return new OrRequestMatcher(getActuatorEndpointStream().toArray(RequestMatcher[]::new));
+        } else {
+            // NOTE: Creating an OrRequest using no Values results in a Runtime Error
+            //       WORKAROUND: Create a negated AnyRequestMatcher
+            return new NegatedRequestMatcher(AnyRequestMatcher.INSTANCE);
+        }
+    }
+
+    private Stream<RequestMatcher> getActuatorEndpointStream() {
+        return getActuatorEndpointStrings().map(AntPathRequestMatcher::new);
+    }
+
+    private Stream<String> getActuatorEndpointStrings() {
+        // Enable the Actuator Endpoints in the Development Environment
+        if (this.profile == StartupConfig.Profile.DEBUG) {
+            return Stream.of("/actuator/**");
+        } else {
+            return Stream.of();
+        }
+    }
+    //endregion
+
+    //region Swagger Endpoint Matchers
+    @Value("${springdoc.api-docs.path}")
+    private String swaggerDocPath;
+
+    @Value("${springdoc.swagger-ui.path}")
+    private String swaggerApiPath;
+
+    private RequestMatcher getSwaggerEndpointMatcher() {
+        // Enable the Swagger Endpoints in the Development Environment
+        if (this.profile == StartupConfig.Profile.DEBUG) {
+            return new OrRequestMatcher(getSwaggerEndpointStream().toArray(RequestMatcher[]::new));
+        } else {
+            // NOTE: Creating an OrRequest using no Values results in a Runtime Error
+            //       WORKAROUND: Create a negated AnyRequestMatcher
+            return new NegatedRequestMatcher(AnyRequestMatcher.INSTANCE);
+        }
+    }
+
+    private Stream<RequestMatcher> getSwaggerEndpointStream() {
+        return getSwaggerEndpointStrings().map(AntPathRequestMatcher::new);
+    }
+
+    private Stream<String> getSwaggerEndpointStrings() {
+        // Enable the Swagger Endpoints in the Development Environment
+        if (this.profile == StartupConfig.Profile.DEBUG) {
+            return Stream.of(swaggerDocPath, swaggerApiPath, "/swagger-ui/index.html");
+        } else {
+            return Stream.of();
+        }
+    }
+    //endregion
+
     //region Filter Chain Bean
     @Bean
     public SecurityFilterChain filterChain(
-            HttpSecurity http, AuthenticationEntryPoint entryPoint,
-            RestAccessDeniedHandler accessDeniedHandler, StartupConfig.Profile activeProfile
+            HttpSecurity http, AuthenticationEntryPoint entryPoint, RestAccessDeniedHandler accessDeniedHandler
     ) throws Exception {
+        RequestMatcher publicMappings = new OrRequestMatcher(
+                getActuatorEndpointMatcher(),
+                getPublicEndpointMatcher(),
+                getSwaggerEndpointMatcher()
+        );
+        RequestMatcher protectedMappings = new NegatedRequestMatcher(publicMappings);
+
         http
                 // Register the custom AuthenticationProvider and AuthenticationFilter
                 .authenticationProvider(provider)
-                .addFilterBefore(bearerAuthenticationFilter(http), AnonymousAuthenticationFilter.class)
-                .addFilterBefore(cookieAuthenticationFilter(http), AnonymousAuthenticationFilter.class)
-                // Specify which Routes/Endpoints should be protected and which ones should be accessible to everyone.
-                .authorizeHttpRequests(auth ->
-                        auth
-                                // Only allow authenticated Users to use the API
-                                .requestMatchers(endpointMatcherUtil.getProtectedApiRoutes()).authenticated()
-                                .requestMatchers(endpointMatcherUtil.getAdminRoutes()).hasAuthority(Permission.ADMIN.toString())
-                                // Permit everyone to get the static resources
-                                .requestMatchers(AnyRequestMatcher.INSTANCE).permitAll()
+                .addFilterBefore(bearerAuthenticationFilter(http, protectedMappings), AnonymousAuthenticationFilter.class)
+
+                .authorizeHttpRequests(auth -> auth
+                            .requestMatchers(publicMappings).permitAll()
+                            .requestMatchers(protectedMappings).authenticated()
                 )
 
                 // Disable the default Web Security Login and Logout Pages.
                 .formLogin().disable()
                 .logout().disable()
+                // Disable Anonymous Users from connecting
+                .anonymous().disable()
         ;
-
-        // Make H2-Console available for testing
-        if (activeProfile.equals(StartupConfig.Profile.TEST)) {
-            http.headers().frameOptions().disable();
-        }
 
         // Register the custom Authentication Entry Point and Access Denied Handler.
         http.exceptionHandling()
@@ -120,3 +207,4 @@ public class SecurityConfiguration {
     }
     //endregion
 }
+
