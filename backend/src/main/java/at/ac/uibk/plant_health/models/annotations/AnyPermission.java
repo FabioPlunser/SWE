a2ec07f27abab064bfa7e0aebf2ab426e4b85b6b
davirieser <david.w.rieser@gmail.com>
2023-03-10 15:41:09 +0100

    Renamed "swa"-Module to "plant_health"

---
+++ backend/src/main/java/at/ac/uibk/plant_health/models/annotations/AnyPermission.java
@@ -0,0 +1,13 @@
+package at.ac.uibk.plant_health.models.annotations;
+
+import at.ac.uibk.plant_health.models.Permission;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Documented
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnyPermission {
+    Permission[] value();
+}
