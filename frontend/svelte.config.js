02906461ae446c68ae15795825704c327d97aa04
eprader <56026248+eprader@users.noreply.github.com>
2023-03-11 14:45:24 +0100

    run prettier hook on all files

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,18 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import tailwind from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess({
     postcss: {
-			plugins: [
-				tailwind, 
-				autoprefixer
-			]
-		}
+      plugins: [tailwind, autoprefixer],
+    },
   }),
   kit: {
     alias: {
