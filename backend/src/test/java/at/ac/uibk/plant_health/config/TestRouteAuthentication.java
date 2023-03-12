936455387d1632fa5f92103b4c5d285dcb302b96
David Werner Rieser <david.w.rieser@student.uibk.ac.at>
2023-03-12 19:03:58 +0100

    Added active Profile "test" to TestRouteAuthentication

--- backend/src/test/java/at/ac/uibk/plant_health/config/TestRouteAuthentication.java
+++ backend/src/test/java/at/ac/uibk/plant_health/config/TestRouteAuthentication.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.ReflectionUtils;
@@ -32,6 +33,7 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class TestRouteAuthentication {
     //region Autowired + Fields
