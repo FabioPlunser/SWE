72ec4f20f2a9e1564b48473c2c629ee09d5f1acf
David Werner Rieser <david.w.rieser@student.uibk.ac.at>
2023-03-12 15:13:01 +0100

    Added Annotation for requiring Principal of certain Type for an Endpoint

---
+++ backend/src/main/java/at/ac/uibk/plant_health/models/annotations/aspects/PrincipalRequiredAspect.java
@@ -0,0 +1,33 @@
+package at.ac.uibk.plant_health.models.annotations.aspects;
+
+import at.ac.uibk.plant_health.models.annotations.PrincipalRequired;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.AccessDeniedException;
+
+@Aspect
+@Component
+public class PrincipalRequiredAspect {
+    @Autowired
+    private HttpServletRequest request;
+
+    @Around("@annotation(at.ac.uibk.plant_health.models.annotations.PrincipalRequired)")
+    public Object doSomething(ProceedingJoinPoint jp) throws Throwable {
+        Class requiredPrinciple = ((MethodSignature) jp.getSignature())
+                                .getMethod()
+                                .getAnnotation(PrincipalRequired.class)
+                                .value();
+
+        if (requiredPrinciple.isAssignableFrom(request.getUserPrincipal().getClass())) {
+            return jp.proceed();
+        }
+
+        throw new AccessDeniedException(String.format("Required Principle {0}", requiredPrinciple.getSimpleName()));
+    }
+}
