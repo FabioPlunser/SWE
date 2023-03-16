4855c16b63bdf02104a85d9b2473788965145d25
David Werner Rieser <david.w.rieser@student.uibk.ac.at>
2023-03-16 23:45:37 +0100

    Minor changes in SensorStation

--- sensor_station/main/main.ino
+++ sensor_station/main/main.ino
@@ -1,12 +1,10 @@
-// https://www.arduino.cc/reference/en/libraries/arduinoble/
 
-// Useful Links:
-// Saving to Flash: https://www.arduino.cc/reference/en/language/variables/utilities/progmem/
-// UUID Generation: https://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_(random)
-// UUID Library: https://github.com/RobTillaart/UUID
+#include "modules/communication.h"
 
-#include <ArduinoBLE.h>
+void setup() {
+	Serial.begin(115200);
+	initialize_communication();
+	enable_pairing_mode();
+}
 
-void setup() { Serial.begin(115200); }
-
-void loop () {}
+void loop() {}
--- sensor_station/main/util/uuid.h
+++ sensor_station/main/util/uuid.h
@@ -11,8 +11,8 @@ typedef struct {
 	uint8_t bits[16];
 } uuid_t;
 
-uuid_t null_uuid(); 
-uuid_t random_uuid(); 
+uuid_t null_uuid();
+uuid_t random_uuid();
 
 void format_uuid(uuid_t uuid, char * string);
 const char * format_uuid_static(uuid_t uuid);
