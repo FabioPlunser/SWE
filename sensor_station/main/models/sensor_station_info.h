f11b3e069f87ce70fc794f5bca421c3851849f57
David Werner Rieser <david.w.rieser@student.uibk.ac.at>
2023-03-16 23:40:26 +0100

    Minor changes in SensorStation Models

--- sensor_station/main/models/sensor_station_info.h
+++ sensor_station/main/models/sensor_station_info.h
@@ -2,23 +2,23 @@
 #ifndef SENSOR_STATION_INFO_H
 #define SENSOR_STATION_INFO_H
 
+#include "../util/uuid.h"
+
 #include <stdbool.h>
 #include <stdint.h>
-
-#include "../util/uuid.h"
 
 #define BATTERY_LEVEL_STATUS_FLAGS 0b010
 
 typedef struct {
-    uint8_t flags;
-    uint16_t power_state;
-    uint16_t battery_level;
+	uint8_t flags;
+	uint16_t power_state;
+	uint16_t battery_level;
 } battery_level_status_t;
 
 typedef struct {
-    uuid_t uuid;
-    battery_level_status_t battery_status;
-    bool unlocked;
+	uuid_t uuid;
+	battery_level_status_t battery_status;
+	bool unlocked;
 } sensor_station_info_t;
 
 #endif
