3507ae4c72ad87af81258448d08c61e80b62a76b
David Werner Rieser <david.w.rieser@student.uibk.ac.at>
2023-03-16 23:41:08 +0100

    Created SensorStation Module for Sensors

--- sensor_station/main/modules/sensors.h
+++ sensor_station/main/modules/sensors.h
@@ -1,10 +1,11 @@
 
-#ifndef SENSORS_H 
-#define SENSORS_H 
+#ifndef SENSORS_H
+#define SENSORS_H
 
 #include "../models/sensor_data.h"
 
 sensor_data_t read_sensor_data();
-void async_read_sensor_data(void (handler*)(sensor_data_t));
+
+void async_read_sensor_data(void (*handler)(sensor_data_t));
 
 #endif
