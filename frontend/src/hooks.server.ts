3a5a7b6f4ca934b2151533e857c41fcf5963a6a7
Fabio Plunser <f.plunser@outlook.com>
2023-03-17 10:23:29 +0100

    update

---
+++ frontend/src/hooks.server.ts
@@ -0,0 +1,41 @@
+import type { Handle } from '@sveltejs/kit';
+import { redirect, error} from '@sveltejs/kit';
+
+
+
+
+export const handle = (async ({ event, resolve }) => {
+
+  const { cookies } = event;
+  let token = cookies.get("token");
+  if(token){
+    token = JSON.parse(token);
+  }else{
+    const response = await resolve(event);
+    return response;
+  }
+
+
+  event.locals.user = token;
+
+  if (event.url.pathname.startsWith("/admin")) {
+    if (event.locals.user.role !== "ADMIN") {
+      throw redirect(307, "/");
+    }
+  }
+
+  if(event.url.pathname.startsWith("/gardener")){
+    if(event.locals.user.role !== "GARDENER"){
+      throw redirect(307, "/");
+    }
+  }
+
+  if(event.url.pathname.startsWith("/user")){
+    if(event.locals.user.role !== "USER"){
+      throw redirect(307, "/");
+    }
+  }
+ 
+  const response = await resolve(event);
+  return response;
+}) satisfies Handle;
