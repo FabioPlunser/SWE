4a8543988f5f4802e8fba7f918a4ae0c8923572f
Fabio Plunser <f.plunser@outlook.com>
2023-03-17 13:58:53 +0100

    added figma svgs

---
+++ frontend/src/lib/components/ui/Icon.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  
+  export let name: string; 
+  export let w: string; 
+  export let h: string;
+  export let shadow: boolean = false;
+  export let css: string = "";
+
+  let icon: any;  
+  onMount(async () => {
+    
+    icon = (await import(`../../assets/icons/${name}.svg?url`)).default;
+    console.log(icon);
+  });
+
+</script>
+
+<!-- @component
+  name: Icon
+  description: Icon component
+  props:
+    name: string
+    w: number
+    h: number
+-->
+
+
+<img src="{icon}" alt="{name}" width="{w}" height="{h}" class="dark:invert {shadow ? "drop-shadow-3xl" : ""} {css}" />
