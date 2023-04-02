bf21e81b7c44f94f99a424253f3631e455ce889b
Emanuel Prader <emanuel.prader@student.uibk.ac.at>
2023-04-02 23:01:04 +0200

    initial pemplate guest page

--- frontend/src/routes/(app)/guest/+page.svelte
+++ frontend/src/routes/(app)/guest/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-
+  export let data;
 </script>
 
-<h1>Guest</h1>
+<h1>Images</h1>
+
+<p>{data.plantName}</p>
+<p>{data.roomName}</p>
+{#each data.pictures as ref}
+  <img alt="Plant image" src={ref} />
+{/each}
+<!--
+-->
