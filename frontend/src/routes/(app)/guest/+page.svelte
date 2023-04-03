c8a2dbca0b88c7295c0109502df234741c91604c
Emanuel Prader <emanuel.prader@student.uibk.ac.at>
2023-04-03 23:06:30 +0200

    add tailwind styling and Camera.svelte for icon button

--- frontend/src/routes/(app)/guest/+page.svelte
+++ frontend/src/routes/(app)/guest/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
   import Carousel from "./Carousel.svelte";
+  import Camera from "$lib/assets/icons/Camera.svelte";
   export let data;
 </script>
 
-<h1>Images</h1>
-
-<p>{data.plantName}</p>
-<p>{data.roomName}</p>
-<!-- TODO: create infinite scroll-->
-<Carousel pictureRefs={data.pictureRefs} />
-<!--
-{#each data.pictureRefs as ref}
-  <img alt="Plant" src={ref} />
-{/each}-->
+<div class="divide-y-4">
+  <div class="flow-root">
+    <div class="text-2xl py-4 float-left">
+      <p>Room: {data.roomName}</p>
+      <p>Plant: {data.plantName}</p>
+    </div>
+    <div>
+      <button class="btn btn-primary float-right mt-8 mr-5">
+        <!-- TODO: find a way to change fill color using 'fill' or 'dark:fill-white'
+          does not seem to work even in Camara.svelte directlx-->
+        <Camera classNames="w-8" />
+      </button>
+    </div>
+  </div>
+  <div class="py-8">
+    <Carousel pictureRefs={data.pictureRefs} />
+  </div>
+</div>
--- frontend/src/routes/(app)/guest/Carousel.svelte
+++ frontend/src/routes/(app)/guest/Carousel.svelte
@@ -2,9 +2,11 @@
   export let pictureRefs: string[];
 </script>
 
-<ul>
+<ul
+  class="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw]"
+>
   {#each pictureRefs as ref}
-    <li>
+    <li class="shrink-0 snap-center">
       <img alt="Plant" src={ref} />
     </li>
   {/each}
