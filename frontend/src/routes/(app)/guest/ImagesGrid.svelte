e6a4dc9302ebe4529c36c7f98e7ea62a4fb969d0
Emanuel Prader <emanuel.prader@student.uibk.ac.at>
2023-04-04 23:51:24 +0200

    change name to be more descriptive

---
+++ frontend/src/routes/(app)/guest/ImagesGrid.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  type Picture = {
+    imageRef: string;
+    creationDate: Date;
+  };
+
+  export let pictures: Picture[];
+</script>
+
+<div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 gap-4">
+  {#each pictures as { imageRef, creationDate }}
+    <div class="shrink-0 snap-center">
+      <img
+        alt="Plant"
+        class="rounded-xl shadow-xl backdrop-blur-2xl"
+        src={imageRef}
+      />
+      <h1 class="flex justify-center">
+        Date: {creationDate.toLocaleDateString()}
+      </h1>
+    </div>
+  {/each}
+</div>
