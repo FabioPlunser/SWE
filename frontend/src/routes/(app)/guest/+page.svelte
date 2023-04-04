959b7c6a6632ffaeeb89ff255c97bc28b337d904
Emanuel Prader <emanuel.prader@student.uibk.ac.at>
2023-04-04 23:54:59 +0200

    incorporate new test data and change styling. modify onFileSelected to add new picture

--- frontend/src/routes/(app)/guest/+page.svelte
+++ frontend/src/routes/(app)/guest/+page.svelte
@@ -1,42 +1,62 @@
 <script lang="ts">
-  import Carousel from "./Carousel.svelte";
+  type Picture = {
+    imageRef: string;
+    creationDate: Date;
+  };
+
+  import ImagesGrid from "./ImagesGrid.svelte";
   import Camera from "$lib/assets/icons/Camera.svg?component";
- 
+
   export let data;
 
-  let  test:any, fileinput:any;
-	
-	function onFileSelected(e: any){
+  let test: any, fileinput: any;
+
+  function onFileSelected(e: any) {
     console.log(e);
     let image = e.target.files[0];
     let reader = new FileReader();
     reader.readAsDataURL(image);
-    reader.onload = e => {
-      test = e?.target?.result
+    reader.onload = (e) => {
+      // TODO: POST to endpoint and add image from response to data.pictures
+      test = e?.target?.result;
+      let response: Picture = {
+        imageRef: "https://picsum.photos/200/300",
+        creationDate: new Date(),
+      };
+      data.pictures.unshift(response);
+      data.pictures = data.pictures;
     };
   }
-
 </script>
 
-<section class="mt-12">
-  <div class="flex justify-center gap-12">
-    <div>
-      <h1 class="text-2xl font-bold">Room: {data.roomName}</h1>
-      <h1 class="text-2xl font-bold">Plant: {data.plantName}</h1>
+<section class="mt-10">
+  <div class="flex justify-between">
+    <div class="text-2xl font-bold">
+      <h1>Room: {data.roomName}</h1>
+      <h1>Plant: {data.plantName}</h1>
     </div>
-      <button on:click={()=>fileinput.click()} class="btn btn-primary h-full w-fit hover:dark:fill-black hover:fill-white">
-        <Camera class="w-24 dark:fill-white" />
-        <input class="hidden" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
-      </button>
+    <button
+      on:click={() => fileinput.click()}
+      class="btn btn-primary mt-3 hover:dark:fill-black hover:fill-white"
+    >
+      <Camera class="w-8 dark:fill-white" />
+      <input
+        class="hidden"
+        type="file"
+        accept=".jpg, .jpeg, .png; capture=camera"
+        on:change={(e) => onFileSelected(e)}
+        bind:this={fileinput}
+      />
+    </button>
   </div>
 
-  <h1>Uploaded Picture</h1>
-  {#if test}
-    <img  src="{test}" alt="uploaded image" />
-  {/if}
+  <div>
+    {#if test}
+      <img src={test} alt="plant" />
+    {/if}
+  </div>
 
-  <div class="mt-12">
-    <Carousel pictures={data.pictures}/>
+  <div class="mt-6">
+    <ImagesGrid pictures={data.pictures} />
   </div>
 </section>
-  
