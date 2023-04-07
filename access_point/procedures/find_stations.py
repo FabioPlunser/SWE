2cb8da471f7c5bf9f432078ec2a1d24baaf608e5
Fabian Margreiter <fabian.margreiter@gmail.com>
2023-04-07 19:32:16 +0200

    48: Slightly improved reporting of new found sensor stations, base structure already implemented during testing of SensorStation class

--- access_point/procedures/find_stations.py
+++ access_point/procedures/find_stations.py
@@ -21,7 +21,7 @@ def find_stations(conf: Config):
     log.info('Starting to scan for sensor stations')
     known_addresses = database.get_all_known_sensor_station_addresses()
     new_stations = scan_for_new_stations(known_addresses, SENSOR_STATION_NAME, timedelta(seconds=10))
-    log.info(f'Found {len(new_stations)} new sensor stations')
+    log.info(f'Found potential {len(new_stations)} new sensor stations')
 
     report_data = []
     for station in new_stations:
@@ -33,8 +33,9 @@ def find_stations(conf: Config):
         except UnableToConnectError:
             log.warning(f'Failed to connect to station "{station.address}"')
 
-    backend.report_found_sensor_station(report_data)
-    log.info(f'Reported found sensor stations to backend')
+    if report_data:
+        backend.report_found_sensor_station(report_data)
+        log.info(f'Reported {len(report_data)} found sensor stations to backend')
 
     conf.update(scan_active=False)
     log.info(f'Disabled scanning mode')
