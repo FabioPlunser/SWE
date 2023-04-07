5506863d92c3276f254fa80093fb34435ff94a24
Emanuel Prader <emanuel.prader@student.uibk.ac.at>
2023-04-07 17:41:05 +0200

    initial styling with dummy data from server.ts

---
+++ frontend/src/routes/(app)/profile/+page.server.ts
@@ -0,0 +1,20 @@
+export async function load({ params, url }) {
+  //let userId = url.searchParams.get("userId");
+
+  // TODO: fetch proper backend endpoint
+  let username: string = "Sakura";
+  let userEmail: string = "sakura.tree@mail.com";
+  let userPassword: string = "1stPasswdOfSakura";
+  let userPermissions: Map<string, boolean> = new Map<string, boolean>([
+    ["user", true],
+    ["gardener", true],
+    ["admin", false],
+  ]);
+
+  return {
+    username,
+    userEmail,
+    userPassword,
+    userPermissions,
+  };
+}
--- frontend/src/routes/(app)/profile/+page.svelte
+++ frontend/src/routes/(app)/profile/+page.svelte
@@ -1,17 +1,69 @@
 <script lang="ts">
   import Input from "$components/ui/Input.svelte";
+  import CheckRing from "$lib/assets/icons/checkRing.svg?component";
+  export let data;
+  let roles: string[] = Array.from(data.userPermissions.keys());
 </script>
 
 <h1>Profile</h1>
 
 <section class="w-full">
-  <div class="flex justify-center items-center w-full ">
+  <form action="" method="get" class="flex justify-center">
     <div class="w-full max-w-md">
-      <Input field="username" label="Username" />
-      <Input field="email" label="Email" />
+      <Input
+        type="text"
+        field="username"
+        label="Username"
+        placeholder="Username"
+        value={data.username}
+      />
+      <Input
+        type="email"
+        field="email"
+        label="Email"
+        placeholder="example.mail@planthealth.com"
+        value={data.userEmail}
+      />
+      <Input
+        type="password"
+        field="password"
+        label="Password"
+        placeholder="Password"
+        value={data.userPassword}
+      />
+      <!--
       <button class="btn btn-primary mt-4 flex justify-center mx-auto"
         >Update</button
       >
+    -->
+      <div class="mt-6">
+        <ul class="grid w-full gap-6 md:grid-rows-3">
+          {#each roles as role}
+            <li class="float-root peer-checked:border-green">
+              {data.userPermissions.get(role)}
+              <input
+                type="checkbox"
+                id={role}
+                value={data.userPermissions.get(role)}
+                class="hidden peer"
+              />
+              <label
+                for={role}
+                class="btn-primary inline-flex w-full p-3 border-2 rounded-2xl peer-checked:border-gray-700 dark:peer-checked:border-white cursor-pointer justify-between"
+              >
+                <div>
+                  <h1 class="text-lg font-semibold capitalize">{role}</h1>
+                </div>
+                <div>
+                  {#if data.userPermissions.get(role)}
+                    <CheckRing class="fill-none" />
+                  {/if}
+                </div>
+              </label>
+            </li>
+          {/each}
+        </ul>
+      </div>
     </div>
-  </div>
+  </form>
 </section>
