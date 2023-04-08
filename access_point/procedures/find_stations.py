81c473d2dc9dcedceae0cbbab5de78f26c3202a9
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-08 15:16:56 +0200

    76: Known sensor stations are checked again at end of scan and removed from results

--- access_point/procedures/find_stations.py
+++ access_point/procedures/find_stations.py
@@ -18,11 +18,13 @@ def find_stations(conf: Config):
     backend = Server(conf.backend_address, conf.token)
     database = Database(DB_FILENAME)
 
+    # start scan, ignore known stations
     log.info('Starting to scan for sensor stations')
     known_addresses = database.get_all_known_sensor_station_addresses()
     new_stations = scan_for_new_stations(known_addresses, SENSOR_STATION_NAME, timedelta(seconds=10))
     log.info(f'Found {len(new_stations)} potential new sensor stations')
 
+    # get required data
     report_data = []
     for station in new_stations:
         try:
@@ -33,9 +35,20 @@ def find_stations(conf: Config):
         except UnableToConnectError:
             log.warning(f'Failed to connect to station {station.address}')
 
+    # remove stations that have been enabled while scanning
+    known_addresses = database.get_all_known_sensor_station_addresses()
+    report_data = [entry for entry in report_data if entry.get('address') not in known_addresses]
+
+    # send data to backend
     if report_data:
-        backend.report_found_sensor_station(report_data)
-        log.info(f'Reported {len(report_data)} found sensor stations to backend')
+        try:
+            backend.report_found_sensor_station(report_data)
+            log.info(f'Reported {len(report_data)} found sensor stations to backend')
+        except ConnectionError as e:
+            log.error(e)
+            return
+    else:
+        log.info('Did not find any new sensor stations to report to backend')
 
     conf.update(scan_active=False)
     log.info(f'Disabled scanning mode')
