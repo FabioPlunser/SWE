080eb0e84d170cfec81b42779e68f80dea8dba62
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-08 23:28:29 +0200

    Changed expected name of sensor station from 'SensorStationG1T1' to 'SensorStation'

--- access_point/util/const.py
+++ access_point/util/const.py
@@ -1,4 +1,4 @@
 # static configuration, not designed to be changed during initialization
 CONFIG_FILENAME = 'conf.yaml'
 DB_FILENAME = 'data.db'
-SENSOR_STATION_NAME = 'SensorStationG1T1'
+SENSOR_STATION_NAME = 'SensorStation'
