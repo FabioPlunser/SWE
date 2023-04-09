8dbf576ffb9cd2390ff0538d1cfdf6af52de6af6
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-09 16:25:54 +0200

    83: Renamed main python file for better identification

--- access_point/stop_access_point.sh
+++ access_point/stop_access_point.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-kill $(ps -aux | grep "python3 main.py" | grep -v grep | awk '{print $2}')
-kill $(ps -aux | grep "start_access_point.sh" | grep -v grep | awk '{print $2}')
+kill -SIGINT $(ps -aux | grep "python3 access_point.py" | grep -v grep | awk '{print $2}')
