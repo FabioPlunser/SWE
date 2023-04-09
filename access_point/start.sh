c58a95ab4b2fc9f97c64dfec4483b6738b951d53
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-09 15:36:04 +0200

    83: Fixed errors after merge conflict

--- access_point/start.sh
+++ access_point/start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
 echo "Activating venv"
 source venv/bin/activate
 echo "Starting program execution"
-=======
-source venv/bin/activate
->>>>>>> 03455d0d4b6573af04a70a999903e01d1835f731
 python3 main.py
