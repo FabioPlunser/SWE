c89d3bd47e145a8907f2f7e39f02bdae92947ff2
Emanuel Prader <emanuel.prader@student.uibk.ac.at>
2023-04-14 01:24:28 +0200

    use PasswordInput component and get started with input validation

--- frontend/src/routes/(app)/profile/+page.server.ts
+++ frontend/src/routes/(app)/profile/+page.server.ts
@@ -1,3 +1,39 @@
+import { fail } from "@sveltejs/kit";
+import { z } from "zod";
+
+const schema = z.object({
+  username: z
+    .string({ required_error: "Username is required" })
+    .min(1, { message: "Username is required" })
+    .max(64, { message: "Username must be less than 64 characters" })
+    .trim(),
+
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(1, { message: "Password is required" })
+    .min(6, { message: "Password must be at least 6 characters" })
+    .max(32, { message: "Password must be less than 32 characters" })
+    .trim(),
+});
+
+export const actions = {
+  default: async ({ request }) => {
+    const formData = await request.formData();
+    const zodData = schema.safeParse(Object.fromEntries(formData));
+    if (!zodData.success) {
+      // Loop through the errors array and create a custom errors array
+      const errors = zodData.error.errors.map((error) => {
+        return {
+          field: error.path[0],
+          message: error.message,
+        };
+      });
+
+      return fail(400, { error: true, errors });
+    }
+  },
+};
+
 export async function load({ params, url }) {
   //let userId = url.searchParams.get("userId");
 
--- frontend/src/routes/(app)/profile/+page.svelte
+++ frontend/src/routes/(app)/profile/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+  import { enhance } from "$app/forms";
+  import FormError from "$helper/formError.svelte";
   import Input from "$components/ui/Input.svelte";
   import CheckRing from "$lib/assets/icons/checkRing.svg?component";
   import BooleanButton from "$lib/components/ui/BooleanButton.svelte";
+  import toast from "$components/toast";
+  import PasswordInput from "$lib/components/ui/PasswordInput.svelte";
 
   export let data;
+
+  export let form;
 
   let roles: string[] = Array.from(Object.keys(data.userPermissions));
 </script>
@@ -11,7 +17,7 @@
 <h1>Profile</h1>
 
 <section class="w-full">
-  <form action="" method="get" class="flex justify-center">
+  <form method="POST" class="flex justify-center" use:enhance>
     <div class="w-full max-w-md">
       <Input
         type="text"
@@ -20,6 +26,7 @@
         placeholder="Username"
         value={data.username}
       />
+      <FormError field="username" {form} />
       <Input
         type="email"
         field="email"
@@ -27,18 +34,10 @@
         placeholder="example.mail@planthealth.com"
         value={data.userEmail}
       />
-      <Input
-        type="password"
-        field="password"
-        label="Password"
-        placeholder="Password"
-        value={data.userPassword}
-      />
-      <!--
-      <button class="btn btn-primary mt-4 flex justify-center mx-auto"
-        >Update</button
-      >
-    -->
+      <FormError field="email" {form} />
+      <PasswordInput field="password" value={data.userPassword} />
+
+      <FormError field="password" {form} />
       <div class="mt-6">
         <ul class="grid w-full gap-6 md:grid-rows-3">
           {#each roles as role}
@@ -50,6 +49,9 @@
             </li>
           {/each}
         </ul>
+      </div>
+      <div class="flex justify-center mt-6">
+        <button type="submit" class="btn btn-primary">submit</button>
       </div>
     </div>
   </form>
