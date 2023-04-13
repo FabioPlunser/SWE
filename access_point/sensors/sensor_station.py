1a81e2142d5abaaa58b158de0c94f3aeda9f4dd0
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-13 21:00:30 +0200

    97: Fixed typo

--- access_point/sensors/sensor_station.py
+++ access_point/sensors/sensor_station.py
@@ -80,7 +80,7 @@ class Sensor:
                 if get_short_uuid(characteristic.uuid) != self.ALARM_CHARACTERISTIC_UUID:
                     try:
                         self.characteristic_uuid = get_short_uuid(characteristic.uuid)
-                        return int.from_bytes(await self.client.read_gatt_char(characteristic), byteorder='big') * self.resolution(self.characteristic_uuid)
+                        return int.from_bytes(await self.client.read_gatt_char(characteristic), byteorder='big') * self.resolution
                     except Exception as e:
                         raise ReadError(f'Unable to read value of sensor {self.name}: {e}')
                     
