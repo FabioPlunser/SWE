83d561bf22c75d2fab56a7654fffd61b936444de
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-13 18:50:15 +0200

    97: Adapted to one service per sensor concept

--- access_point/sensors/sensor_station.py
+++ access_point/sensors/sensor_station.py
@@ -2,7 +2,7 @@ import bleak
 import asyncio
 
 from bleak import BleakClient, exc
-from typing import Optional, Literal
+from typing import Optional, Literal, Callable
 
 # Fixing wrong definition of 'Battery Level Status' characteristic UUID
 bleak.uuids.uuid16_dict[0x2BED] = 'Battery Level State'
@@ -42,65 +42,19 @@ def get_short_uuid(uuid: str) -> str:
     """
     return uuid[4:8]
 
-class SensorDefinition:
-    """
-    Definition of a sensor. Does not hold any actual sensor data
-    but is used to convert raw sensor values into actual measurements.
-    """
-    def __init__(self, name: str, unit: str, resolution: float) -> None:
-        """
-        Sets the sensor definition.
-        :param name: Name of the sensor
-        :param unit: Unit of value measured by the sensor
-        :param resolution: Resolution of the sensor - sensors provide integer output.
-        """
-        self.name = name
-        self.unit = unit
-        self.resolution = resolution
-
-    def bytes_to_value(self, bytes: bytearray) -> float:
-        """
-        Converts the raw output of a sensor into an actual measurement.
-        """
-        if not bytes:
-            return None
-        return int.from_bytes(bytes) * self.resolution
-
-class SensorStation:
-    """
-    Handler class for a sensor station.
-    """
-    SERVICE_UUIDS = [
-        'dea07cc4-d084-11ed-a760-325096b39f47',
-        'dea07cc4-d084-11ed-a760-325096b39f48'
-    ]
-
-    SENSORS = [
-        SensorDefinition('Earth Humidity', '%', 0.01),
-        SensorDefinition('Air Humidity', '%', 0.01),
-        SensorDefinition('Air Pressure', 'Pa', 0.1),
-        SensorDefinition('Temperature', '°C', 0.5),
-        SensorDefinition('Air Quality', '%', 0.5),
-        SensorDefinition('Light Intensity', 'lm', 1),
-        SensorDefinition('Battery Level', '%', 1)
-    ]
-
-    # UUID of characteristics for setting alarms for sensors
-    ALARM_UUID = '2a9a'
+class Sensor:
+    ALARM_CHARACTERISTIC_UUID = '2a9a'
     ALARM_CODES = {
         'n': 0,
         'l': 1,
         'h': 2
     }
 
-    def __init__(self, address:str, client: BleakClient = None) -> None:
-        """
-        Initializes a sensor station.
-        :param address: The address of the sensor station
-        :param client: The BleakClient used to communicate with the sensor
-        """
-        self.address = address
-        self.client: BleakClient = client
+    def __init__(self, name: str, service_uuid: str, client: BleakClient, transform: Callable = lambda x: x ) -> None:
+        self.name = name
+        self.service_uuid = service_uuid
+        self.client = client
+        self.transform = transform
 
     def _with_connection(f):
         """
@@ -115,79 +69,140 @@ class SensorStation:
         return decorated
     
     @_with_connection
-    async def _get_characteristic(self,
-                                  description: Optional[str] = None,
-                                  id: Optional[str] = None,
-                                  ignore_id: Optional[str] = None) -> Optional[bleak.BleakGATTCharacteristic]:
-        """
-        Internal method for getting a characteristic from a sensor station.
-        Use _read_characteristic() or _write_characteristic() to read or write
-        the actual value.
-        :param description: Description of the characteristic to find
-        :param id: UUID (2 bytes) of the characteristic to find
-        :param ignore_id: Characteristics with this UUID (2 bytes) will be ignored
-        :return: The first matching characteristic or None of nothing has been found
-        :raises ValueError: If neither description nor id are provided
-        :raises ValueError: If id and ignore_id are identical
-        """
-        if not description and not id:
-            raise ValueError('Either description or id required')
-        if id and id == ignore_id:
-            raise ValueError('Id and ignore_id must not be identical')
+    async def get_value(self) -> float:
         for service in self.client.services:
-            # skip irrelevant services
-            if service.uuid not in self.SERVICE_UUIDS:
+            # service uuid not matching
+            if service.uuid != self.service_uuid:
                 continue
-            # iterate through characteristics and find matching description
+            
             for characteristic in service.characteristics:
-                if ((not description or characteristic.description == description) and
-                    (not id or get_short_uuid(characteristic.uuid) == id) and
-                    (not ignore_id or get_short_uuid(characteristic.uuid) != ignore_id)):
-                    return characteristic
+                # ignore alarm characteristic
+                if get_short_uuid(characteristic.uuid) != self.ALARM_CHARACTERISTIC_UUID:
+                    try:
+                        self.characteristic_uuid = get_short_uuid(characteristic.uuid)
+                        raw_value = await int.from_bytes(await self.client.read_gatt_char(characteristic))
+                        return self.transform(raw_value) * self.resolution(characteristic.uuid) 
+                    except Exception as e:
+                        raise ReadError(f'Unable to read value of sensor {self.name}: {e}')
+                    
+            raise ReadError(f'Unable to find characteristic for value of sensor {self.name}')
+
+        raise ReadError(f'Unable to find service {self.service_uuid} for sensor {self.name}')
     
     @_with_connection
-    async def _read_characteristic(self,
-                                   description: Optional[str] = None,
-                                   id: Optional[str] = None,
-                                   ignore_id: Optional[str] = None) -> bytearray:
-        """
-        Reads the value of a characteristic from the sensor station.
-        :param description: Description of the characteristic to find
-        :param id: UUID (2 bytes) of the characteristic to find
-        :param ignore_id: Characteristics with this UUID (2 bytes) will be ignored
-        :return: The raw value of the characteristic
-        :raises ValueError: If neither description nor id are provided
-        :raises ValueError: If id and ignore_id are identical
-        :raises ReadError: If there was an error reading the value of the characteristic
-        """
-        characteristic = await self._get_characteristic(description, id, ignore_id)
-        try:
-            return await self.client.read_gatt_char(characteristic)
-        except Exception as e:
-            raise ReadError(f'Unable to read characteristic {description if description else id}{( "(id != " + ignore_id + ")") if ignore_id else ""}: {e}')
+    async def set_alarm(self, alarm: Literal['n', 'l', 'h']) -> None:
+        for service in self.client.services:
+            # service uuid not matching
+            if service.uuid != self.service_uuid:
+                continue
+
+            for characteristic in service.characteristics:
+                # ignore everything but alarm characteristic
+                if get_short_uuid(characteristic.uuid) == self.ALARM_CHARACTERISTIC_UUID:
+                    try:
+                        return await self.client.write_gatt_char(characteristic, data=self.ALARM_CODES[alarm].to_bytes())
+                    except Exception as e:
+                        raise WriteError(f'Unable to write alarm for sensor {self.name} on station {self.client.address}: {e}')
                     
+            raise WriteError(f'Unable to find characteristic for alarm of sensor {self.name} on station {self.client.address}')
+        
+        raise WriteError(f'Unable to find service {self.service_uuid} for sensor {self.name} on station {self.client.address}')
+    
+    @property
+    def resolution(self) -> float:
+        if self.characteristic_uuid:
+            resolutions = {
+                '2a6f': 0.01,
+                '2a6d': 0.1,
+                '2b0d': 0.5,
+                '2b04': 0.5,
+                '2aff': 1,
+                '2bed': 1
+            }
+            return resolutions.get(get_short_uuid(self.characteristic_uuid))
+        else:
+            raise ReadError(f'Unable to determine resolution of sensor {self.name} on station {self.client.address} - read sensor data first')
+
+    @property
+    def unit(self) -> str:
+        if self.characteristic_uuid:
+            units = {
+                '2a6f': '%',
+                '2a6d': 'Pa',
+                '2b0d': '°C',
+                '2b04': '%',
+                '2aff': 'lm',
+                '2bed': '%'
+            }
+            return units.get(get_short_uuid(self.characteristic_uuid))
+        else:
+            raise ReadError(f'Unable to determine unit of sensor {self.name} on station {self.client.address} - read sensor data first')
+
+class SensorStation:
+    """
+    Handler class for a sensor station.
+    """
+    INFO_SERVICE_UUID = 'dea07cc4-d084-11ed-a760-325096b39f47'
+    SENSOR_DATA_READ_SERVICE_UUID = 'dea07cc4-d084-11ed-a760-325096b39f48'
+    SENSOR_DATA_SERVICE_UUIDS = {
+        'Earth Humidity': 'dea07cc4-d084-11ed-a760-325096b39f49',
+        'Air Humidity': 'dea07cc4-d084-11ed-a760-325096b39f50',
+        'Air Pressure': 'dea07cc4-d084-11ed-a760-325096b39f51',
+        'Temperature': 'dea07cc4-d084-11ed-a760-325096b39f52',
+        'Air Quality': 'dea07cc4-d084-11ed-a760-325096b39f53',
+        'Light Intensity': 'dea07cc4-d084-11ed-a760-325096b39f54'
+    }
+
+    def __init__(self, address:str, client: BleakClient = None) -> None:
+        """
+        Initializes a sensor station.
+        :param address: The address of the sensor station
+        :param client: The BleakClient used to communicate with the sensor
+        """
+        self.address = address
+        self.client: BleakClient = client
+        self.sensors = [Sensor(sensor_name, service_uuid, self.client)
+                        for sensor_name, service_uuid in self.SENSOR_DATA_SERVICE_UUIDS.items()]
+
+    def _with_connection(f):
+        """
+        Internal decorator method to check if the BleakClient is properly set.
+        Raises a NoConnectionError if not.
+        """
+        async def decorated(self, *args, **kwargs):
+            if self.client and self.client.is_connected:
+                return await f(self, *args, **kwargs)
+            else:
+                raise NoConnectionError
+        return decorated
+    
     @_with_connection
-    async def _write_characteristic(self,
-                                    description: Optional[str] = None,
-                                    id: Optional[str] = None,
-                                    ignore_id: Optional[str] = None,
-                                    data: bytearray = b'\0') -> None:
-        """
-        Writes the value of a characteristic on the sensor station.
-        :param description: Description of the characteristic to find
-        :param id: UUID (2 bytes) of the characteristic to find
-        :param ignore_id: Characteristics with this UUID (2 bytes) will be ignored
-        :param data: Raw data to write to the characteristic value
-        :return: The raw value of the characteristic
-        :raises ValueError: If neither description nor id are provided
-        :raises ValueError: If id and ignore_id are identical
-        :raises WriteError: If there was an error writing the value of the characteristic
-        """
-        characteristic = await self._get_characteristic(description, id, ignore_id)
-        try:
-            await self.client.write_gatt_char(characteristic, data)
-        except Exception as e:
-            raise WriteError(f'Unable to write characteristic {description if description else id}{( "(id != " + ignore_id + ")") if ignore_id else ""}: {e}')
+    async def _read_characteristic(self, service_uuid: str, characteristic_uuid: str) -> bytearray:
+        for service in self.client.services:
+            if service.uuid != service_uuid:
+                continue
+
+            for characteristic in service.characteristics:
+                if get_short_uuid(characteristic.uuid) != characteristic_uuid:
+                    return await self.client.read_gatt_char(characteristic)
+                
+            raise ReadError(f'Characteristic {characteristic_uuid} not found on service {service_uuid} on station {self.address}')
+        
+        raise ReadError(f'Service {service_uuid} not found on statin {self.address}')
+    
+    @_with_connection
+    async def _write_characteristic(self, service_uuid: str, characteristic_uuid: str, data: bytearray) -> None:
+        for service in self.client.services:
+            if service.uuid != service_uuid:
+                continue
+
+            for characteristic in service.characteristics:
+                if get_short_uuid(characteristic.uuid) != characteristic_uuid:
+                    return await self.client.write_gatt_char(characteristic, data)
+                
+            raise ReadError(f'Characteristic {characteristic_uuid} not found on service {service_uuid} on station {self.address}')
+        
+        raise ReadError(f'Service {service_uuid} not found on statin {self.address}')
     
     @property
     async def dip_id(self) -> int:
@@ -196,7 +211,8 @@ class SensorStation:
         :raises ReadError: If it was not possible to read the DIP switch position
         :raises NoConnectionError: If the BleakClient was not properly initialized
         """
-        return int.from_bytes(await self._read_characteristic(description='DIP-Switch'))
+        return int.from_bytes(await self._read_characteristic(service_uuid=self.INFO_SERVICE_UUID,
+                                                              characteristic_uuid='2a9a'))
     
     @property
     async def unlocked(self) -> bool:
@@ -205,7 +221,8 @@ class SensorStation:
         :raises ReadError: If it was not possible to read the flag
         :raises NoConnectionError: If the BleakClient was not properly initialized 
         """
-        return bool.from_bytes(await self._read_characteristic(description='Unlocked'))
+        return bool.from_bytes(await self._read_characteristic(service_uuid=self.INFO_SERVICE_UUID,
+                                                               characteristic_uuid='2ae2'))
     
     async def set_unlocked(self, value: bool) -> None:
         """
@@ -214,7 +231,9 @@ class SensorStation:
         :raises WriteError: If it was not possible to write the flag
         :raises NoConnectionError: If the BleakClient was not properly initialized
         """
-        await self._write_characteristic(description='Unlocked', data=value.to_bytes())
+        await self._write_characteristic(service_uuid=self.INFO_SERVICE_UUID,
+                                         characteristic_uuid='2ae2',
+                                         data=value.to_bytes())
     
     @property
     async def sensor_data_read(self):
@@ -224,7 +243,8 @@ class SensorStation:
         :raises ReadError: If it was not possible to read the flag
         :raises NoConnectionError: If the BleakClient was not properly initialized
         """
-        return bool.from_bytes(await self._read_characteristic(description='Sensor Data Read'))
+        return bool.from_bytes(await self._read_characteristic(service_uuid=self.SENSOR_DATA_READ_SERVICE_UUID,
+                                                               characteristic_uuid='2ae2'))
     
     async def set_sensor_data_read(self, value: bool):
         """
@@ -233,7 +253,9 @@ class SensorStation:
         :raises WriteError: If it was not possible to write the flag
         :raises NoConnectionError: If the BleakClient was not properly initialized
         """
-        await self._write_characteristic(description='Sensor Data Read', data=value.to_bytes())
+        await self._write_characteristic(service_uuid=self.SENSOR_DATA_READ_SERVICE_UUID,
+                                         characteristic_uuid='2ae2',
+                                         data=value.to_bytes())
 
     @property
     async def sensor_data(self) -> dict[str, float]:
@@ -245,12 +267,9 @@ class SensorStation:
         :raises NoConnectionError: If the BleakClient was not properly initialized
         """
         values = {}
-        for sensor in self.SENSORS:
+        for sensor in self.sensors:
             try:
-                if sensor.name == 'Battery Level':
-                    values[sensor.name] = sensor.bytes_to_value(await self._battery_level_bytes)
-                else:
-                    values[sensor.name] = sensor.bytes_to_value(await self._read_characteristic(description=sensor.name, ignore_id=self.ALARM_UUID))
+                values[sensor.name] = await sensor.get_value()
             except ReadError:
                 # ignore read errors on sensor data -> skip over currently unreadable sensor values
                 pass
@@ -276,26 +295,6 @@ class SensorStation:
                 pos = 3 + 2 * flags['IdentifierPresent']
                 return battery_level_status[pos:pos+1]
         return None
-
-    @property
-    async def alarms(self) -> dict[str, int]:
-        """
-        Reads the current state of alarms set on the sensor station.
-        Unreadable alarms will be ignored.
-        :return: A dictionary with the sensor names as keys and the alarm states
-            0 -> No alarm
-            1 -> Lower limit exceeded
-            2 -> Upper limit exceeded
-        :raises NoConnectionError: If the BleakClient was not properly initialized
-        """
-        alarms = {}
-        for sensor in self.SENSORS:
-            try:
-                alarms[sensor.name] = int.from_bytes(await self._read_characteristic(description=sensor.name, id=self.ALARM_UUID))
-            except ReadError:
-                # ignore read errors on alarms -> skip over currently unreadable alarms
-                pass
-        return alarms
     
     async def set_alarms(self, alarms: dict[str, Literal['n', 'l', 'h']]) -> None:
         """
@@ -306,24 +305,24 @@ class SensorStation:
             'h' -> Upper limit exceeded
         :raises NoConnectionError: If the BleakClient was not properly initialized
         """
-        for sensor, alarm in alarms.items():
-            if sensor not in [s.name for s in self.SENSORS]:
-                raise ValueError(f'Sensor {sensor} is not known - alarm cannot be set')
-            try:
-                await self._write_characteristic(description=sensor, id=self.ALARM_UUID, data=self.ALARM_CODES[alarm].to_bytes())
-            except WriteError:
-                pass
-            except KeyError:
-                raise ValueError(f'Alarm flags must be "n", "l" or "h"')
+        for sensor_name, alarm in alarms.items():
+            for sensor in self.sensors:
+                if sensor.name != sensor_name:
+                    continue
+                try:
+                    await sensor.set_alarm(alarm)
+                except WriteError:
+                    pass
+                except KeyError:
+                    raise ValueError(f'Alarm flags must be "n", "l" or "h"')
             
-    @classmethod
-    def get_sensor_unit(cls, sensor_name: str) -> str:
+    def get_sensor_unit(self, sensor_name: str) -> str:
         """
         Returns the unit for a specific sensor.
         :param sensor_name: The name of the sensor
         :return: The unit in which the measured values are
         """
-        for sensor in cls.SENSORS:
+        for sensor in self.sensors:
             if sensor.name == sensor_name:
                 return sensor.unit
         return None
