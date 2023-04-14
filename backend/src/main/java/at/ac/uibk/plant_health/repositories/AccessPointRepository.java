980a5a4783128f82b2749f6f58cb411c97a04dc5
Fabio Plunser <f.plunser@outlook.com>
2023-04-14 21:53:10 +0200

    updated models

--- backend/src/main/java/at/ac/uibk/plant_health/repositories/AccessPointRepository.java
+++ backend/src/main/java/at/ac/uibk/plant_health/repositories/AccessPointRepository.java
@@ -9,12 +9,16 @@ import java.util.UUID;
 import at.ac.uibk.plant_health.models.device.AccessPoint;
 import at.ac.uibk.plant_health.models.user.Person;
 
+import javax.swing.text.html.Option;
+
 public interface AccessPointRepository extends CrudRepository<AccessPoint, UUID> {
 	@Override
 	List<AccessPoint> findAll();
 
 	@Override
 	Optional<AccessPoint> findById(UUID deviceId);
+
+	Optional<AccessPoint> findBySelfAssignedId(UUID selfAssignedId);
 
 	Optional<AccessPoint> findByRoomName(String roomName);
 }
--- backend/src/test/java/at/ac/uibk/plant_health/models/TestAccessPointModel.java
+++ backend/src/test/java/at/ac/uibk/plant_health/models/TestAccessPointModel.java
@@ -2,7 +2,6 @@ package at.ac.uibk.plant_health.models;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.hibernate.Hibernate;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -37,6 +36,7 @@ public class TestAccessPointModel {
 		AccessPoint accessPoint = new AccessPoint(UUID.randomUUID(), "TestRoom", 10, false);
 		accessPointRepository.save(accessPoint);
 
+		assertEquals(accessPoint.getDeviceId(), accessPointRepository.findById(accessPoint.getDeviceId()).get().getDeviceId());
 		assertFalse(accessPointRepository.findAll().isEmpty());
 	}
 
@@ -48,9 +48,83 @@ public class TestAccessPointModel {
 		accessPointRepository.save(accessPoint);
 
 		assertEquals(selfAssignedId, accessPoint.getSelfAssignedId());
-		AccessPoint savedAccessPoint =
-				accessPointRepository.findById(accessPoint.getDeviceId()).get();
+
+		AccessPoint savedAccessPoint = accessPointRepository.findById(accessPoint.getDeviceId()).get();
 		assertEquals(accessPoint.getDeviceId(), savedAccessPoint.getDeviceId());
 		assertEquals(accessPoint.getSelfAssignedId(), savedAccessPoint.getSelfAssignedId());
+
+		// unlock access point search for sensor stations
+		savedAccessPoint.setUnlocked(true);
+		savedAccessPoint.setScanActive(true);
+		accessPointRepository.save(savedAccessPoint);
+
+		assertTrue(accessPointRepository.findById(accessPoint.getDeviceId()).get().isUnlocked());
+		assertTrue(accessPointRepository.findById(accessPoint.getDeviceId()).get().isScanActive());
+	}
+
+	@Test
+	@Transactional
+	void addFoundSensorStations(){
+		// given registered AccessPoint, found SensorStations
+		AccessPoint accessPoint = new AccessPoint(UUID.randomUUID(), "TestRoom", 10, false);
+		accessPointRepository.save(accessPoint);
+
+		// given list of found sensorStations
+		List<SensorStation> sensorStations = new ArrayList<>();
+		for(int i = 0; i < 5; i++){
+			String BD_ADDR = String.format("11:22:33:44:55:6%d", i);
+			sensorStations.add(new SensorStation(BD_ADDR, 255-i));
+		}
+
+		// set accessPoint per SensorStation
+		for(SensorStation ss : sensorStations){
+			ss.setAccessPoint(accessPoint);
+			sensorStationRepository.save(ss);
+		}
+
+		// set sensorStations of accessPoint
+		AccessPoint foundAccessPoint = accessPointRepository.findById(accessPoint.getDeviceId()).get();
+		foundAccessPoint.setSensorStations(sensorStations);
+		accessPointRepository.save(foundAccessPoint);
+
+		assertEquals(sensorStations, accessPointRepository.findById(accessPoint.getDeviceId()).get().getSensorStations());
+	}
+
+	@Test
+	@Transactional
+	void removeSensorStationsFromAccessPoint(){
+		// given registered AccessPoint, found SensorStations
+		AccessPoint accessPoint = new AccessPoint(UUID.randomUUID(), "TestRoom", 10, false);
+		accessPointRepository.save(accessPoint);
+
+		// given list of found sensorStations
+		List<SensorStation> sensorStations = new ArrayList<>();
+		for(int i = 0; i < 5; i++){
+			String BD_ADDR = String.format("11:22:33:44:55:6%d", i);
+			sensorStations.add(new SensorStation(BD_ADDR, 255-i));
+		}
+
+		// set accessPoint per SensorStation
+		for(SensorStation ss : sensorStations){
+			ss.setAccessPoint(accessPoint);
+			sensorStationRepository.save(ss);
+		}
+
+		// set sensorStations of accessPoint
+		AccessPoint foundAccessPoint = accessPointRepository.findById(accessPoint.getDeviceId()).get();
+		foundAccessPoint.setSensorStations(sensorStations);
+		accessPointRepository.save(foundAccessPoint);
+
+		// remove sensor station from access point
+		AccessPoint foundAccessPoint2 = accessPointRepository.findById(accessPoint.getDeviceId()).get();
+		foundAccessPoint2.getSensorStations().remove(2);
+		foundAccessPoint2.getSensorStations().get(2).setAccessPoint(null);
+		accessPointRepository.save(foundAccessPoint2);
+
+		assertEquals(4, accessPointRepository.findById(accessPoint.getDeviceId()).get().getSensorStations().size());
+		assertEquals(5, sensorStationRepository.findAll().size());
+
 	}
 }
+
+
