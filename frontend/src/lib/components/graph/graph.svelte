1fc1cc2342ea359c44daa5f082dd0074064b7bd9
Fabio Plunser <f.plunser@outlook.com>
2023-04-15 12:03:22 +0200

    minor changes

--- frontend/src/lib/components/graph/graph.svelte
+++ frontend/src/lib/components/graph/graph.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from "svelte";
   let dispatch = createEventDispatcher();
-  
-  import {fly} from "svelte/transition";
+
+  import { fly } from "svelte/transition";
 
   import { Line } from "./index";
   import Trash from "$assets/icons/trash.svg?component";
@@ -44,12 +44,10 @@
 -->
 
 <div
- class="bg-base-100 dark:bg-white/30 backdrop-blur-2xl rounded-2xl shadow-2xl p-2 w-fit h-fit" >
-  <button
-    class="absolute top-0 right-0 m-2"
-    on:click={() => remove()}
-  >
-    <i class="bi bi-trash text-3xl hover:text-primary"></i>
+  class="bg-base-100 dark:bg-white/30 backdrop-blur-2xl rounded-2xl shadow-2xl p-2 w-fit h-fit"
+>
+  <button class="absolute top-0 right-0 m-2" on:click={() => remove()}>
+    <i class="bi bi-trash text-3xl hover:text-primary shadow-2xl" />
   </button>
   <br class="mt-1" />
   <h1 class="flex justify-center text-2xl">Plant 1</h1>
@@ -58,27 +56,57 @@
       <div class="">
         <Line width={500} height={300} data={test?.[graph]} />
       </div>
-      <div class="bg-green-400 mx-2 my-auto shadow-2xl rounded-2xl grid grid-rows gap-4 p-4 h-min">
+      <div
+        class="bg-green-400 mx-2 my-auto  shadow-2xl rounded-3xl grid grid-rows gap-4 p-4 h-min"
+      >
         <button
           class="flex justify-center"
           on:click={() => setGraph(graphType.Temperature)}
         >
-          <i class="bi bi-thermometer-half hover:text-white dark:text-white hover:dark:text-black text-3xl {graph === graphType.Temperature ? "text-blue-400" : ""}"></i>
+          <i
+            class="bi bi-thermometer-half hover:text-white dark:text-white hover:dark:text-black text-3xl {graph ===
+            graphType.Temperature
+              ? 'text-blue-400'
+              : ''}"
+          />
         </button>
         <button on:click={() => setGraph(graphType.Humidity)}>
-          <i class="bi bi-droplet-half hover:text-white dark:text-white hover:dark:text-black text-3xl {graph === graphType.Humidity ? "text-blue-500" : ""}"></i>
+          <i
+            class="bi bi-droplet-half hover:text-white dark:text-white hover:dark:text-black text-3xl {graph ===
+            graphType.Humidity
+              ? 'text-blue-500'
+              : ''}"
+          />
         </button>
         <button on:click={() => setGraph(graphType.Light)}>
-          <i class="bi bi-brightness-high-fill hover:text-white dark:text-white hover:dark:text-black text-3xl {graph === graphType.Light ? "text-yellow-400 font-bold" : ""}"></i>
+          <i
+            class="bi bi-brightness-high-fill hover:text-white dark:text-white hover:dark:text-black text-3xl {graph ===
+            graphType.Light
+              ? 'text-yellow-400 font-bold'
+              : ''}"
+          />
         </button>
         <button on:click={() => setGraph(graphType.Soil)}>
-          <i class="bi bi-box-fill hover:text-white dark:text-white hover:dark:text-black text-3xl {graph === graphType.Soil ? "text-slate-400" : ""}"></i>
+          <i
+            class="bi bi-box-fill hover:text-white dark:text-white hover:dark:text-black text-3xl {graph ===
+            graphType.Soil
+              ? 'text-slate-400'
+              : ''}"
+          />
         </button>
       </div>
     </div>
     <div class="absolute right-0 bottom-0 gap-4 flex items-center mr-2 mt-8">
-        <button><i class="bi bi-calendar-event-fill text-2xl hover:text-primary shadow-2xl"/></button>
-        <button><i class="bi bi-card-image text-3xl hover:text-primary shadow-2xl"/></button>
+      <button
+        ><i
+          class="bi bi-calendar-event-fill text-2xl hover:text-primary shadow-2xl"
+        /></button
+      >
+      <button
+        ><i
+          class="bi bi-card-image text-3xl hover:text-primary shadow-2xl"
+        /></button
+      >
     </div>
   </div>
 </div>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,12 @@
 import type { UserConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
-import svg from '@poppanator/sveltekit-svg'
-
+import svg from "@poppanator/sveltekit-svg";
 
 const config: UserConfig = {
   plugins: [sveltekit(), svg()],
-  ssr:{
-    noExternal:['chart.js'],
-  },
+  // ssr:{
+  //   noExternal:['chart.js'],
+  // },
 };
 
 export default config;
